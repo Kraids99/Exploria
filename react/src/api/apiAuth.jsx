20514d00800e25b9a4dffa684e59eee4813b7222
Kraids99 <alexanderwongso99@gmail.com>
2025-11-21 11:15:18 +0700

    Update apiAuth.jsx

---
+++ react/src/api/apiAuth.jsx
@@ -0,0 +1,30 @@
+import useAxios from "./index.jsx";
+
+const SignUpCustomer = async (data) => {
+  try {
+    const response = await useAxios.post("/register/customer", data);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+const SignUpAdmin = async (data) => {
+  try {
+    const response = await useAxios.post("/register/admin", data);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+const SignIn = async (data) => {
+  try {
+    const response = await useAxios.post("/login", data);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export { SignUpCustomer, SignUpAdmin, SignIn };
