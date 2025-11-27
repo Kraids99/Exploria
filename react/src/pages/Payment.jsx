e566f3e36a4b592a1c66367b4ecf5fae50665d6c
AdelaYolis <230712240@students.uajy.ac.id>
2025-11-27 18:45:39 +0700

    tailwind2

---
+++ react/src/pages/Payment.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/landingpage/Navbar";
+import Footer from "../components/landingpage/Footer";
+
+function Payment() {
+    return (
+        <div className="min-h-screen bg-amber-50 font-sans">
+            <Navbar />
+
+            <div className="w-full h-64 md:h-80 overflow-hidden">
+                
+            </div>
+        </div>
+    )
+}
